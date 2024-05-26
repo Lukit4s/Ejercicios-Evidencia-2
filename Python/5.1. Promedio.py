d74a9be7f684c60241f617c4853d495838196e20
Lukit4s <lucas.dylan.ledesma@gmail.com>
2024-05-26 20:30:27 -0300

    Update 5.1. Promedio.py

--- Python/5.1. Promedio.py
+++ Python/5.1. Promedio.py
@@ -1,1 +1,10 @@
+print("Esta cursando cinco materias, ingrese la nota de cada una de ellas")
+nota1=float(input("Ingrese la primera nota: "))
+nota2=float(input("Ingrese la segunda nota: "))
+nota3=float(input("Ingrese la tercera nota: "))
+nota4=float(input("Ingrese la cuarta nota: "))
+nota5=float(input("Ingrese la quinta nota: "))
 
+suma= nota1+nota2+nota3+nota4+nota5
+promedio= suma / 5
+print("Su promedio seria: ",promedio)
